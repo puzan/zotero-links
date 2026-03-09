eb94c841a28fb8dd58b4504dc931c1baa77d1cc3
Ilya Zonov <izonov@gmail.com>
2026-03-09 15:33:29 +0300

    ci: Add release workflow and versioned XPI build

--- build.sh
+++ build.sh
@@ -2,9 +2,19 @@
 set -e
 
 PLUGIN_NAME="zotero-links"
-OUTPUT="${PLUGIN_NAME}.xpi"
+VERSION="${1:-}"
 
-rm -f "$OUTPUT"
-zip -r "$OUTPUT" manifest.json bootstrap.js
+if [ -n "$VERSION" ]; then
+  OUTPUT="$(pwd)/${PLUGIN_NAME}-${VERSION}.xpi"
+  TMPDIR=$(mktemp -d)
+  trap 'rm -rf "$TMPDIR"' EXIT
+  jq --arg v "$VERSION" '.version = $v' manifest.json > "$TMPDIR/manifest.json"
+  cp bootstrap.js "$TMPDIR/bootstrap.js"
+  (cd "$TMPDIR" && zip "$OUTPUT" manifest.json bootstrap.js)
+else
+  OUTPUT="${PLUGIN_NAME}.xpi"
+  rm -f "$OUTPUT"
+  zip "$OUTPUT" manifest.json bootstrap.js
+fi
 
 echo "Built: $OUTPUT"
